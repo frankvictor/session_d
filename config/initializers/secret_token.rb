a2ecbf08afe3d1645fa130bee89212b0fad311b1
frank <frankvictor@qq.com>
2012-11-25 19:47:07 +0800

    new empty project with only 'rails new session_d'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SessionD::Application.config.secret_token = '8015b18c6e57761bd2f7044aa06338a1d12b8e743004d9e5d3290370bf0968dc823afbdd181c576c94050bd48348bc01446c02e2e0d7eaef1f52ab6f0c105475'
